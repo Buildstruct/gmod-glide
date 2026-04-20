747353768c0d1e195c855d68d7b5d63a5875d8c6
Bero <129704530+ThatBero@users.noreply.github.com>
2026-04-20 17:33:19 -0500

    buff

--- lua/entities/glide_projectile_launcher.lua
+++ lua/entities/glide_projectile_launcher.lua
@@ -90,8 +90,8 @@ function ENT:Initialize()
     self.projectileLifetime = 5
 
     self.reloadDelay = 1
-    self.explosionRadius = 350
-    self.explosionDamage = 100
+    self.explosionRadius = 530
+    self.explosionDamage = 950
     self.smokeColor = Vector( 80, 80, 80 )
 
     self.projectileModel = "models/props_phx/misc/flakshell_big.mdl"
